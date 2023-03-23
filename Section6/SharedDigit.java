fb7963b701346735bc2897d48027057ce8300871
Zaky <zakyymuh123@gmail.com>
2023-03-23 12:38:43 +0700

    Shared Digit Optimalize

--- Section6/SharedDigit.java
+++ Section6/SharedDigit.java
@@ -8,18 +8,8 @@ public class SharedDigit {
   public static boolean hasSharedDigit(int number1, int number2) {
     if(number1 < 10 || number1 > 99 || number2 < 10 || number2 > 99){
       return false;
+    } else {
+      return (number1 / 10 == number2 / 10) || (number1 / 10 == number2 % 10 ) || (number1 % 10 == number2 / 10) || (number1 % 10 == number2 % 10);
     }
-
-    int firstDigit = number1 % 10;
-    int secondDigit = number1 / 10 % 10;
-
-    while(number2 > 0){
-      if(number2 % 10 == firstDigit || number2 % 10 == secondDigit){
-        return true;
-      }
-      number2 /= 10;
-    }
-
-    return false;
   }
 }
