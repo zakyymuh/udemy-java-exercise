5ba07feed6d9897933b2f0b29f0eee4abee15653
Zaky <zakyymuh123@gmail.com>
2023-03-23 11:13:55 +0700

    Challenge Even Digit Sum

---
+++ Section6/EvenDigitSum.java
@@ -0,0 +1,24 @@
+package Section6;
+
+public class EvenDigitSum {
+  public static void main(String[] args) {
+    System.out.println("Sum of even digit number from 124690 is " + getEvenDigitSum(124690));
+  }
+
+  public static int getEvenDigitSum(int number) {
+    if(number < 0) return -1;
+    int totalSum = 0;
+    int lastDigit;
+    while(number > 0){
+      lastDigit = number % 10;
+      if(lastDigit % 2 == 0){
+        totalSum += lastDigit;
+      }
+
+      number /= 10;
+    }
+
+    return totalSum;
+
+  }
+}
