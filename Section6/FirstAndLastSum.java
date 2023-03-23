cd3d957cd75c8bd400000caadd51c541b7c645d8
Zaky <zakyymuh123@gmail.com>
2023-03-23 10:56:44 +0700

    Challenge First&Last digit sum

---
+++ Section6/FirstAndLastSum.java
@@ -0,0 +1,21 @@
+package Section6;
+
+public class FirstAndLastSum {
+  public static void main(String[] args) {
+    System.out.println("Sum of first and last digit from 204 = " + sumFirstAndLastDigit(204));
+  }
+
+  public static int sumFirstAndLastDigit(int number) {
+    if(number < 0){
+      return -1;
+    }
+    int lastDigit = number % 10;
+    
+    while(number > 9){
+      number /= 10;
+    }
+
+    int firstDigit = number % 10;
+    return firstDigit + lastDigit;
+  }
+}
