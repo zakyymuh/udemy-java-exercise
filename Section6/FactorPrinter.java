a79e266f9e49a49566117dc078a59e279daef3c7
Zaky <zakyymuh123@gmail.com>
2023-03-24 13:06:16 +0700

    Optimalize

--- Section6/FactorPrinter.java
+++ Section6/FactorPrinter.java
@@ -8,10 +8,21 @@ public class FactorPrinter {
   public static void printFactors(int number) {
     if(number < 1) System.out.println("Invalid Value");
 
-    for(int i = 1; i <= number; i++){
+
+    //old
+    // for(int i = 1; i <= number; i++){
+    //   if(number % i == 0){
+    //     System.out.println(i);
+    //   }
+    // }  
+
+    //optimize
+    for(int i = 1; i <= number / 2; i++){
       if(number % i == 0){
         System.out.println(i);
       }
     }  
+
+    System.out.println(number);
   }
 }
