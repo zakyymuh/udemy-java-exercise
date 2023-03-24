b3273c75ea04f2f48b3893c25d60e27602b949fd
Zaky <zakyymuh123@gmail.com>
2023-03-24 13:26:49 +0700

    Perfect Number Finale

---
+++ Section6/PerfectNumber.java
@@ -0,0 +1,20 @@
+package Section6;
+
+public class PerfectNumber {
+  public static void main(String[] args) {
+    System.out.println("6 is " + (isPerfectNumber(6) ? "" : "not ") + "perfect number");
+  }
+
+  public static boolean isPerfectNumber(int number) {
+    if(number < 1) return false;
+    
+    int sum = 0;
+    for(int i = 1; i <= number / 2; i++){
+      if(number % i == 0){
+        sum += i;
+      }
+    }
+    
+    return sum == number ? true : false;
+  }
+}
