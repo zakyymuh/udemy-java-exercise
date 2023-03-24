436fbdf370ccd680fe3c24a5fc9af09aea87a324
Zaky <zakyymuh123@gmail.com>
2023-03-24 13:20:33 +0700

    Another Solution

--- Section6/GreatestCommonDivisor.java
+++ Section6/GreatestCommonDivisor.java
@@ -6,16 +6,29 @@ public class GreatestCommonDivisor {
   }
 
   public static int getGreatestCommonDivisor(int first, int second) {
-    if(first < 10 || second < 10) return -1;
+    if (first < 10 || second < 10)
+      return -1;
+    // my solution
+    // int biggest = (first > second) ? first : second;
+    // int common = 1;
+    // for(int i = 1; i <= biggest; i++){
+    // if(first % i == 0 && second % i == 0){
+    // common = i;
+    // }
+    // }
 
-    int biggest = (first > second) ? first : second;
-    int common = 1;
-    for(int i = 1; i <= biggest; i++){
-      if(first % i == 0 && second % i == 0){
-        common = i;
+    // return common;
+
+    // internet solution
+    while (first != second) {
+      if (first > second){
+        first = first - second;
+        System.out.println(first);
+      } else {
+        second = second - first;
+        System.out.println(second);
       }
     }
-
-    return common;
+    return second;
   }
 }
