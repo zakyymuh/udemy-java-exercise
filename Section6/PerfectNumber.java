ef082ca5abce78246ad903d5ddd20d7a7fef26df
Zaky <zakyymuh123@gmail.com>
2023-03-24 13:29:55 +0700

    Optimalize Solution

--- Section6/PerfectNumber.java
+++ Section6/PerfectNumber.java
@@ -6,15 +6,25 @@ public class PerfectNumber {
   }
 
   public static boolean isPerfectNumber(int number) {
-    if(number < 1) return false;
+    // my own solution
+    // if(number < 1) return false;
     
+    // int sum = 0;
+    // for(int i = 1; i <= number / 2; i++){
+    //   if(number % i == 0){
+    //     sum += i;
+    //   }
+    // }
+    
+    // return sum == number ? true : false;
+
+    //optimalize solution
     int sum = 0;
-    for(int i = 1; i <= number / 2; i++){
-      if(number % i == 0){
+    for(int i = 1; i < number; i++){
+      if(number % i == 0)
         sum += i;
-      }
     }
-    
-    return sum == number ? true : false;
+
+    return sum == number && number > 1;
   }
 }
